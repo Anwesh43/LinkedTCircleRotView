810bdfed46b8aa35674635bf22368e36bd61552c
Anwesh43 <anweshthecool0@gmail.com>
2019-05-29 09:42:52 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtcirclerotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtcirclerotview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtcirclerotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tcirclerotview.TCircleRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TCircleRotView.create(this)
     }
 }
