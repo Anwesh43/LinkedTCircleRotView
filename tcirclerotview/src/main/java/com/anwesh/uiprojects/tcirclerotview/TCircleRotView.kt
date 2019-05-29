e5acc130fa34c48a71159835a76b84d55ad238d0
Anwesh43 <anweshthecool0@gmail.com>
2019-05-29 09:13:12 +0530

    adding constants and libraries

--- tcirclerotview/src/main/java/com/anwesh/uiprojects/tcirclerotview/TCircleRotView.kt
+++ tcirclerotview/src/main/java/com/anwesh/uiprojects/tcirclerotview/TCircleRotView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.tcirclerotview
 /**
  * Created by anweshmishra on 29/05/19.
  */
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Int = 5
+val startDeg : Float = -90f
+val sweepDeg : Float = 360f
