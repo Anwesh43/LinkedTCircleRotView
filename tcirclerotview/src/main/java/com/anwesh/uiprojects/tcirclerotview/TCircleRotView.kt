3514dc5825de00ec6bff45cfb7d6d6c91077008d
Anwesh43 <anweshthecool0@gmail.com>
2019-05-29 09:36:57 +0530

    adding TCircleRot class

--- tcirclerotview/src/main/java/com/anwesh/uiprojects/tcirclerotview/TCircleRotView.kt
+++ tcirclerotview/src/main/java/com/anwesh/uiprojects/tcirclerotview/TCircleRotView.kt
@@ -176,7 +176,32 @@ class TCircleRotView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
         }
     }
+
+    data class TCircleRot(var i : Int) {
+
+        private val root : TCRNode = TCRNode(0)
+        private var curr : TCRNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
+
 }
